f20d459a0376ae56d46f4017506919daa84012ca
notna4 <cristicanceal@gmail.com>
2023-04-03 13:09:59 +0300

    initial commit

--- src/main/java/com/example/corealproject/HelloApplication.java
+++ src/main/java/com/example/corealproject/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("hola mundo!!");
+        Scene scene = new Scene(fxmlLoader.load(), 720, 420);
+        stage.setTitle("Bestest program");
         stage.setScene(scene);
         stage.show();
     }
